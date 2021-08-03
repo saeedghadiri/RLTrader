6afedf6d984d3a84a4df133d15e0a9fe0635c3f6
Saeed Ghadiri <saeed.ghadiri@gmail.com>
2021-08-03 20:54:23 +0430

    add support for dow 30 stocks - read from local pickle if file already exists.

--- apps/RLTrader/config.py
+++ apps/RLTrader/config.py
@@ -1,12 +1,24 @@
-TICKERS = ['AMZN', 'MCD', 'DRI', 'F', 'ADM', 'COST', 'PEP', 'WMT', 'ABBV',
-           'AMGN', 'COO', 'JNJ', 'AAL', 'CAT', 'EMR', 'ROP', 'AAPL', 'IBM',
-           'MA', 'PYPL', 'COG', 'EOG', 'HP', 'XOM', 'AIG', 'BAC', 'JPM',
-           'USB', 'CMS', 'ED', 'DUK', 'PCG', 'DIS', 'V', 'HD', 'KO', 'FB',
-           'INTC', 'TMUS', 'C', 'PM', 'MS', 'SBUX', 'GS', 'T', 'AMT', 'CVS',
-           'AXP', 'BLK', 'SHOP', 'SAP', 'NKE', 'TM', 'RY', 'GOOGL', 'NFLX',
-           'AIV', 'BXP', 'DRE', 'IRM', 'ALB', 'IFF', 'PKG', 'IP', 'VZ', 'LIN',
-           'NEE', 'BA', 'TD', 'BTC-USD', 'ETH-USD', 'BNB-USD', 'XRP-USD',
-           'LTC-USD', 'ADA-USD', 'DOGE-USD', 'BCH-USD', 'XLM-USD', 'ETC-USD',
-           'XMR-USD', 'TRX-USD', 'EURUSD=X', 'GBPUSD=X', 'JPY=X', 'NZDUSD=X',
-           'AUDUSD=X', '^FVX', '^TNX', '^TYX', '^IRX', 'GC=F', 'CL=F', 'ZC=F',
-           'CT=F', 'PL=F', 'SI=F', 'HG=F', 'RB=F', 'HO=F', 'NG=F']
+tickers = 'DOW30'
+
+ALL_TICKERS = ['AMZN', 'MCD', 'DRI', 'F', 'ADM', 'COST', 'PEP', 'WMT', 'ABBV',
+               'AMGN', 'COO', 'JNJ', 'AAL', 'CAT', 'EMR', 'ROP', 'AAPL', 'IBM',
+               'MA', 'PYPL', 'COG', 'EOG', 'HP', 'XOM', 'AIG', 'BAC', 'JPM',
+               'USB', 'CMS', 'ED', 'DUK', 'PCG', 'DIS', 'V', 'HD', 'KO', 'FB',
+               'INTC', 'TMUS', 'C', 'PM', 'MS', 'SBUX', 'GS', 'T', 'AMT', 'CVS',
+               'AXP', 'BLK', 'SHOP', 'SAP', 'NKE', 'TM', 'RY', 'GOOGL', 'NFLX',
+               'AIV', 'BXP', 'DRE', 'IRM', 'ALB', 'IFF', 'PKG', 'IP', 'VZ', 'LIN',
+               'NEE', 'BA', 'TD', 'BTC-USD', 'ETH-USD', 'BNB-USD', 'XRP-USD',
+               'LTC-USD', 'ADA-USD', 'DOGE-USD', 'BCH-USD', 'XLM-USD', 'ETC-USD',
+               'XMR-USD', 'TRX-USD', 'EURUSD=X', 'GBPUSD=X', 'JPY=X', 'NZDUSD=X',
+               'AUDUSD=X', '^FVX', '^TNX', '^TYX', '^IRX', 'GC=F', 'CL=F', 'ZC=F',
+               'CT=F', 'PL=F', 'SI=F', 'HG=F', 'RB=F', 'HO=F', 'NG=F']
+
+DOW_30_TICKERS = ['AAPL', 'MSFT', 'JPM', 'V', 'RTX', 'PG', 'GS', 'NKE', 'DIS', 'AXP', 'HD', 'INTC', 'WMT', 'IBM', 'MRK',
+                  'UNH', 'KO', 'CAT', 'TRV', 'JNJ', 'CVX', 'MCD', 'VZ', 'CSCO', 'XOM', 'BA', 'MMM', 'PFE', 'WBA', 'DD']
+
+if tickers == 'ALL':
+    TICKERS = ALL_TICKERS
+elif tickers == 'DOW30':
+    TICKERS = DOW_30_TICKERS
+
+DATA_PATH = '{}.pkl'.format(tickers)
--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from preprocessor.yahoodownloader import YahooDownloader
 from apps.RLTrader import config
 import pandas as pd
+import os.path
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    df = YahooDownloader(start_date='2016-01-01', end_date='2021-01-01', ticker_list=config.TICKERS).fetch_data()
-    df.to_pickle('data.pkl')
+
+    if not os.path.exists(config.DATA_PATH):
+        df = YahooDownloader(start_date='2016-01-01', end_date='2021-01-01', ticker_list=config.TICKERS).fetch_data()
+        df.to_pickle(config.DATA_PATH)
+    else:
+        df = pd.read_pickle(config.DATA_PATH)
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
