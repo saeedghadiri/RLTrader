07e9605ffc5a063213234f7a15d0b31782bb39f5
Saeed Ghadiri <saeed.ghadiri@gmail.com>
2021-08-03 20:41:45 +0430

    refactor code and add config file for tickers

---
+++ apps/RLTrader/config.py
@@ -0,0 +1,12 @@
+TICKERS = ['AMZN', 'MCD', 'DRI', 'F', 'ADM', 'COST', 'PEP', 'WMT', 'ABBV',
+           'AMGN', 'COO', 'JNJ', 'AAL', 'CAT', 'EMR', 'ROP', 'AAPL', 'IBM',
+           'MA', 'PYPL', 'COG', 'EOG', 'HP', 'XOM', 'AIG', 'BAC', 'JPM',
+           'USB', 'CMS', 'ED', 'DUK', 'PCG', 'DIS', 'V', 'HD', 'KO', 'FB',
+           'INTC', 'TMUS', 'C', 'PM', 'MS', 'SBUX', 'GS', 'T', 'AMT', 'CVS',
+           'AXP', 'BLK', 'SHOP', 'SAP', 'NKE', 'TM', 'RY', 'GOOGL', 'NFLX',
+           'AIV', 'BXP', 'DRE', 'IRM', 'ALB', 'IFF', 'PKG', 'IP', 'VZ', 'LIN',
+           'NEE', 'BA', 'TD', 'BTC-USD', 'ETH-USD', 'BNB-USD', 'XRP-USD',
+           'LTC-USD', 'ADA-USD', 'DOGE-USD', 'BCH-USD', 'XLM-USD', 'ETC-USD',
+           'XMR-USD', 'TRX-USD', 'EURUSD=X', 'GBPUSD=X', 'JPY=X', 'NZDUSD=X',
+           'AUDUSD=X', '^FVX', '^TNX', '^TYX', '^IRX', 'GC=F', 'CL=F', 'ZC=F',
+           'CT=F', 'PL=F', 'SI=F', 'HG=F', 'RB=F', 'HO=F', 'NG=F']
--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 from preprocessor.yahoodownloader import YahooDownloader
+from apps.RLTrader import config
 import pandas as pd
 
-tickers = []
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    tickers = pd.read_csv('Stocks 100c.csv', header=None)
-    tickers[0] = tickers[0].apply(lambda x: x.replace('"', ''))
-    tickers = tickers[0].values
-
-    df = YahooDownloader(start_date='2016-01-01', end_date='2021-01-01', ticker_list=tickers).fetch_data()
+    df = YahooDownloader(start_date='2016-01-01', end_date='2021-01-01', ticker_list=config.TICKERS).fetch_data()
     df.to_pickle('data.pkl')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
